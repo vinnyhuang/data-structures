cd951a3b3128920af846f218842f68994a2e7021
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-09 16:22:43 -0700

    Completed hashTable.js, pass all non-advanced tests.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,24 +3,35 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, {});
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index)[k] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var temp = this._storage.get(index)[k];
+  this._storage.get(index)[k] = undefined;
+  return temp;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: O(1)
+ retrieve: O(1)
+ remove: O(1)
  */
 
 
