16c9b1a29993bbf9c637e0c2e2719e234822dee5
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-09 11:29:52 -0700

    Completed set.js, pass all public tests.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage[item] === undefined) {
+    this._storage[item] = true;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Presumably (depending on Javascript Object implementation), each of the functions run in constant time.
  */
