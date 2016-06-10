023b8e0fae0e2be5d0f5f2b69ba5efa48f1677b3
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-09 18:52:10 -0700

    Setup skeleton of binarySearchTree.js, completed insert.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,48 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.left = null;
+  newTree.right = null;
+  newTree.value = value;
+
+  extend(newTree, treeMethods);
+  
+  return newTree;
+};
+
+treeMethods = {};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+treeMethods.insert = function(value) {
+  if (value <= this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+treeMethods.contains = function(value) {
+
+  
+
+};
+
+treeMethods.depthFirstLog = function(value) {
+
+  
+
 };
 
 
